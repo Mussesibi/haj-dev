cdd2b89f9df4218f4d456b9a3fe6a107f13e9153
yazan <yazanfdhl@gmail.com>
2025-02-03 02:08:25 +0530

    home layour structure done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import "./globals.css";
 import Header from "./componentss/header/page";
-
+import Main from "./componentss/main/page";
+import Carousel from "./componentss/carousel/carousel";
 export default function Home() {
   return (
     <main>
        <Header />
+       {/* <Main /> */}
+       {/* <Carousel /> */}
     </main>
   );
 }
